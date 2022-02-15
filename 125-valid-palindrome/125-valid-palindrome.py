c5293b21b69f39d93cdde6aadba2e95f3bcb6c41
ARAVINDHAN T <arvindhant01@gmail.com>
2022-02-15 12:14:35 +0530

    Time: 74 ms (39.20%), Space: 14 MB (91.17%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -4,12 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        x=''
-        for i in s:
-            if(i.isalnum()):
-                x+=i
-        x=x.lower()
-        
-        return(x==x[::-1])
+        l=0
+        r=len(s)-1
+        while(l<r):
+            while l<r and not s[l].isalnum():
+                l+=1
+            while l<r and not s[r].isalnum():
+                r-=1
+            if s[l].lower()!=s[r].lower():
+                return False
+            l+=1
+            r-=1
+            
+        return True
     
         
