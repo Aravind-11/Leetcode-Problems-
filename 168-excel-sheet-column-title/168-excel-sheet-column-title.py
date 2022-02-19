de553aafb03a5cda67cc9248e8772139a8e4acbc
ARAVINDHAN T <arvindhant01@gmail.com>
2022-02-19 17:19:53 +0530

    Time: 23 ms (45.92%), Space: 13.5 MB (15.02%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.py
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.py
@@ -5,40 +5,12 @@ class Solution(object):
         :rtype: str
         """
         
-        alpha='abcdefghijklmnopqrstuvwxyz'
-        a={}
-        for i in alpha.upper():
-            a[ord(i)-64]=i
-        
-        #if(columnNumber<=26):
-        #    return a[columnNumber]
-        x=[]
-        flag=0
-        if(columnNumber<=26):
-            return a[columnNumber]
-        while(columnNumber>1):
-            num1=columnNumber%26
-            if(flag==1):
-                
-                num1-=1
-                flag=0
-                
-            if(num1==0):
-                flag=1
-                num1=26
-                
-            
-                
-            #print('num1',num1)
-            
-            columnNumber=int(columnNumber/26)
-            #print('c',columnNumber)
-            
-            x.insert(0,a[int(num1)])
-            #print(x)
-            
-        if(columnNumber>=1 and flag!=1):
-            #print('came here')
-            x.insert(0,a[int(columnNumber)])
-            
-        return "".join(x)
+        ans=''
+    
+        while True:
+            columnNumber-=1
+            rem=columnNumber%26
+            columnNumber//=26
+            ans+=chr(rem+ord('A'))
+            if(columnNumber==0):
+                return ans[::-1]
