343b05c0abf090c3c04be1f0c5a505610e074e0f
ARAVINDHAN T <arvindhant01@gmail.com>
2022-02-20 09:16:41 +0530

    Time: 26 ms (58.42%), Space: 13.5 MB (58.11%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.py
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.py
@@ -8,9 +8,9 @@ class Solution(object):
         columnnumber=[]
         for i in range(len(columnTitle)):
             columnnumber.append(26**(i))
-        columnTitle=columnTitle[::-1]
-        for i in range(len(columnTitle)):
-            columnnumber[i]=columnnumber[i]+(26**i*(ord(columnTitle[i])-ord('A')))
+    
+        for i in range(len(columnTitle)-1,-1,-1):
+            columnnumber[i]=columnnumber[i]+(26**(len(columnTitle)-1-i)*(ord(columnTitle[i])-ord('A')))
         return sum(columnnumber)
             
         
