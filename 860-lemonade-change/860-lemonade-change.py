ee1cfc66b4a0d7f3e6f2f0fa441647dc0e405c5b
ARAVINDHAN T <arvindhant01@gmail.com>
2022-02-22 07:55:08 +0530

    Time: 692 ms (95.83%), Space: 17.2 MB (57.50%) - LeetHub

--- 860-lemonade-change/860-lemonade-change.py
+++ 860-lemonade-change/860-lemonade-change.py
@@ -15,31 +15,28 @@ class Solution(object):
         for i in bills:
             if(i==5):
                 a[5]+=1
-                print('a[5]')
+                
             elif(i==10):
                 if(a[5]==0):
                     return False
                 else:
-                    print('a[10]')
                     a[10]+=1
                     a[5]-=1
             else:
-                print('here')
-                print(a[10],a[5])
                 if(a[10]>=1 and a[5]>=1):
-                    print('1')
+                    
                     a[10]-=1
                     a[5]-=1
                     a[20]+=1
                     
                 elif(a[10]==0 and a[5]>=3):
-                    print('2')
+                    
                     a[20]+=1
                     a[5]-=3
                     
                 else:
                     
-                    print('3')
+                
                     return False
                     
                     
