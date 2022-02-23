670802bec4a00697ee3b52313e0342e4a6641406
ARAVINDHAN T <arvindhant01@gmail.com>
2022-02-23 09:44:36 +0530

    Time: 25 ms (68.32%), Space: 13.6 MB (38.36%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -10,10 +10,10 @@ class Solution(object):
         count=0
         oddFound=False
         for i,j in enumerate(a):
-            if(a[j]%2==0):
-                count+=a[j]
+            if(a[j]%2==0): # if frequency is even, then it can be made into a palindrome
+                count+=a[j] 
             else:
-                count+=a[j]-1
+                count+=a[j]-1 # if frequency is odd, we remove one element from the freque
                 oddFound=True
         return count+1 if oddFound else count
                 
