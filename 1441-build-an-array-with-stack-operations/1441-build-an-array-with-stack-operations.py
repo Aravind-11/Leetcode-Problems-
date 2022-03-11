118b9228ced7875bfd7413be03d59e63fa9f511e
ARAVINDHAN T <arvindhant01@gmail.com>
2022-03-11 08:25:16 +0530

    Time: 25 ms (52.42%), Space: 13.8 MB (15.32%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -11,7 +11,8 @@ class Solution(object):
         for i in range(1,n+1):
             beg_list.append(i)
         x=0
-        for i in range(len(beg_list)):
+        g=len(beg_list)
+        for i in range(g):
             if x==len(target):
                 break
             check_list.append(beg_list[i])
